5241d1b7aeb09f26f5e3889eb3778ea6876363cb
rkdtnlzl <rkdtnlzl@naver.com>
2023-04-02 13:48:19 +0900

    initial setting

---
+++ CaZait-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaZait-iOS
+//
+//  Created by 강석호 on 2023/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
