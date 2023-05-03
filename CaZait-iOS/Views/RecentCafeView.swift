1a8dbbf49644f6890eb297e00f58000089fddf93
rkdtnlzl <rkdtnlzl@naver.com>
2023-05-03 15:50:35 +0900

    [Fix] RecentCafeView StatusBar 하얀색으로 지정

--- CaZait-iOS/Views/RecentCafeView.swift
+++ CaZait-iOS/Views/RecentCafeView.swift
@@ -11,19 +11,29 @@ import UIKit
 class RecentCafeView: UIViewController {
     
     
+    private let pinkView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 255, g: 223, b: 217)
+        return view
+    }()
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
+        // 네비게이션컨트롤러를 통해서 Status Bar 색깔 변경
+        self.navigationController?.navigationBar.barStyle = .black
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = UIColor(r: 255, g: 223, b: 217)
         self.view.backgroundColor = .black
         
         // 네비게이션 바 타이틀 설정
         self.title = "최근 본 매장"
+        
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         
         // 뒤로가기 버튼 추가
@@ -34,12 +44,15 @@ class RecentCafeView: UIViewController {
         self.navigationController?.navigationBar.tintColor = .white
         
         
+        self.view.addSubview(pinkView)
+        
+        self.pinkView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
+        
     }
-    // 근데 지금 적용이 안됨...
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent // 밝은 배경색일 경우에는 .darkContent
-    }
-    
     
     @objc func backButtonTapped() {
         self.navigationController?.popViewController(animated: true)
