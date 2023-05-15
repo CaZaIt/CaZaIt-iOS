d73090a7710c47bfa31d22ec3df6614b4d1f9204
rkdtnlzl <rkdtnlzl@naver.com>
2023-05-15 19:09:46 +0900

    [Fix] RecentView CollectionViewCell 파일 생성 후 코드 정리

--- CaZait-iOS/Views/RecentCafeView.swift
+++ CaZait-iOS/Views/RecentCafeView.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class RecentCafeView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class RecentCafeView: UIViewController {
     
     
     private let pinkView: UIView = {
@@ -39,6 +39,10 @@ class RecentCafeView: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
         self.view.backgroundColor = .black
         
+        collectionView.register(RecentCafeCollectionViewCell.self, forCellWithReuseIdentifier: "RecentCafeCollectionViewCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         // 네비게이션 바 타이틀 설정
         self.title = "최근 본 매장"
         
@@ -61,9 +65,6 @@ class RecentCafeView: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
         
         self.pinkView.addSubview(collectionView)
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
         
         self.collectionView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
@@ -71,111 +72,6 @@ class RecentCafeView: UIViewController, UICollectionViewDelegate, UICollectionVi
             make.bottom.equalTo(self.pinkView.snp.bottom).inset(20)
         }
         
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10 // example number of items
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        cell.backgroundColor = .white
-        cell.layer.cornerRadius = 10
-        cell.layer.masksToBounds = true // cornerRadius가 제대로 표시되도록 설정
-        
-        let CafeImageView = UIImageView(frame: cell.contentView.bounds)
-        CafeImageView.image = UIImage(named: "small_cafe2")
-        CafeImageView.contentMode = .scaleAspectFill
-        CafeImageView.clipsToBounds = true
-        
-        let CafeNameLabel = UILabel()
-        CafeNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        CafeNameLabel.text = "롬곡"
-        CafeNameLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
-        CafeNameLabel.textColor = .black
-        
-        let DistanceLabel = UILabel()
-        DistanceLabel.translatesAutoresizingMaskIntoConstraints = false
-        DistanceLabel.text = "220m"
-        DistanceLabel.font = UIFont.systemFont(ofSize: 12)
-        DistanceLabel.textColor = .black
-        
-        
-        let AddressLabel = UILabel()
-        AddressLabel.translatesAutoresizingMaskIntoConstraints = false
-        AddressLabel.text = "서울특별시 광진구 광나루로\n375-1 2층(군자동)"
-        AddressLabel.font = UIFont.systemFont(ofSize: 12)
-        AddressLabel.textColor = .black
-        AddressLabel.numberOfLines = 0 // 여러 줄로 표시 가능하도록 설정
-        AddressLabel.lineBreakMode = .byWordWrapping // 단어 단위로 줄바꿈 설정
-        
-        let CongestionView = UIView()
-        CongestionView.backgroundColor = UIColor(red: 1, green: 0.45, blue: 0.356, alpha: 1)
-        CongestionView.layer.cornerRadius = 20
-        
-        let CongestionLabel = UILabel()
-        CongestionLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        CongestionLabel.textColor = .white
-        CongestionLabel.textAlignment = .center
-        CongestionLabel.text = "보통"
-        CongestionLabel.numberOfLines = 1
-        
-        cell.contentView.addSubview(CafeImageView)
-        cell.contentView.addSubview(CafeNameLabel)
-        cell.contentView.addSubview(DistanceLabel)
-        cell.contentView.addSubview(AddressLabel)
-        cell.contentView.addSubview(CongestionView)
-        cell.contentView.addSubview(CongestionLabel)
-        
-        
-        CafeImageView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(15)
-            make.leading.equalToSuperview().offset(15)
-            make.trailing.equalToSuperview().inset(213)
-            make.bottom.equalToSuperview().inset(20)
-        }
-        
-        CafeNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(CafeImageView.snp.top).inset(10)
-            make.leading.equalTo(CafeImageView.snp.trailing).offset(18)
-        }
-        
-        DistanceLabel.snp.makeConstraints { make in
-            make.top.equalTo(CafeImageView.snp.top).inset(20)
-            make.leading.equalTo(CafeNameLabel.snp.trailing).offset(10)
-        }
-        
-        AddressLabel.snp.makeConstraints { make in
-            make.top.equalTo(CafeNameLabel.snp.bottom).offset(7)
-            make.leading.equalTo(CafeImageView.snp.trailing).offset(18)
-        }
-        
-        CongestionView.snp.makeConstraints { make in
-            make.top.equalTo(AddressLabel.snp.bottom).offset(26)
-            make.leading.equalTo(CafeImageView.snp.trailing).offset(18)
-            make.bottom.equalTo(cell.snp.bottom).inset(20)
-            make.trailing.equalTo(cell.snp.trailing).inset(18)
-        }
-        
-        CongestionLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(CongestionView.snp.centerX)
-            make.centerY.equalTo(CongestionView.snp.centerY)
-        }
-        
-        return cell
-    }
-    
-    // MARK: - UICollectionViewDelegateFlowLayout
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.bounds.width
-        return CGSize(width: width-40, height: 172) // example item size
-    }
-    
-    
-    //셀 간격
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 17 // example line spacing
     }
     
     
@@ -188,6 +84,35 @@ class RecentCafeView: UIViewController, UICollectionViewDelegate, UICollectionVi
         self.navigationController?.isNavigationBarHidden = true
     }
     
+}
+
+extension RecentCafeView: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
+    // collectionView 셀 개수 반환
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // collectionView 셀 생성 및 반환
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecentCafeCollectionViewCell", for: indexPath) as! RecentCafeCollectionViewCell
+        
+        
+        return cell
+    }
+    
+    // collectionView 셀 크기 반환
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width
+        return CGSize(width: width-40, height: 172) // example item size
+    }
+    
+    // collectionView 셀과 셀 사이 간격 반환
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 17
+    }
+
     
 }
+
+
