255e0c4947ad528b917912d0a175dd9ff392a22a
alstn38 <alstn38@naver.com>
2023-05-19 15:20:33 +0900

    [Feat] SearchTableView SearchTableViewCell 추가

--- CaZait-iOS/Views/SearchView.swift
+++ CaZait-iOS/Views/SearchView.swift
@@ -10,26 +10,61 @@ import UIKit
 
 class SearchView: UIViewController {
     
-    let searchBar = UISearchBar()
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        
+        searchBar.searchTextField.textColor = UIColor.white
+        
+        return searchBar
+    }()
+    
+    private let searchTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        
+        tableView.allowsSelection = true
+        tableView.showsVerticalScrollIndicator = false //수직 스크롤 인디게이터를 보이지 않게 함
+        tableView.backgroundColor = .yellow
+        tableView.sectionHeaderTopPadding = 0 //상단 패딩을 0으로 지정한다.
+        
+        return tableView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
         searchBar.delegate = self
         
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        searchTableView.register(SearchTableViewCell.self, forCellReuseIdentifier: "SearchTableViewCell")
+        
+        setupNavigation()
+        setupSearchTableView()
+    }
+    
+    func setupNavigation() {
         // 네비게이션 바에 UISearchBar 추가
         navigationItem.titleView = searchBar
-        searchBar.searchTextField.textColor = UIColor.white
         
         let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonTapped))
         navigationItem.leftBarButtonItem = backButton
         
-        // UISearchController 설정
-        view.backgroundColor = .white
         
         // 내비게이션 바 스타일 변경
         self.navigationController?.navigationBar.backgroundColor = .black
         self.navigationController?.navigationBar.tintColor = .white
+    }
+    
+    func setupSearchTableView() {
+        view.addSubview(searchTableView)
+        
+        searchTableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -89,4 +124,34 @@ extension SearchView: UISearchBarDelegate{
         }
         // 예를 들어 API 호출, 데이터베이스 쿼리 등을 수행할 수 있습니다.
     }
+}
+
+extension SearchView: UITableViewDelegate, UITableViewDataSource{
+    
+    //mainTableView의 섹션의 갯수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    //mainTableView의 각 섹션 마다 cell row 숫자의 갯수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    //mainTableView의 각 센션 마다 사용할 cell의 종류
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
+        cell.navigationController = navigationController
+        
+        return cell
+    }
+
+    //mainTableViewCell의 높이를 지정한다.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 50
+    }
+    
+    
 }
