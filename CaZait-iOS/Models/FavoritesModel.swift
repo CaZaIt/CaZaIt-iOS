0db4b940f70549de9f18b5135b464f03a0746a19
alstn38 <alstn38@naver.com>
2023-07-04 17:07:39 +0900

    [Add] 찜한매장 API, Model, URL 추가

---
+++ CaZait-iOS/Models/FavoritesModel.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesModel.swift
+//  CaZait-iOS
+//
+//  Created by 강민수 on 2023/07/04.
+//
+
+import Foundation
+
+// MARK: - Hello
+struct FavoritesResponse: Codable {
+    let code: Int
+    let result, message: String
+    let data: [FavoritesInfo]
+}
+
+// MARK: - Datum
+struct FavoritesInfo: Codable {
+    let favoritesID, cafeID: Int
+    let name, address, latitude, longitude: String
+    let congestion, imageURL: String
+}
