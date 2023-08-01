449150a8c1d8b365e749c276748046c59d7b76c3
rkdtnlzl <rkdtnlzl@naver.com>
2023-08-01 21:34:11 +0900

    [Feat]NoticeView & CustomerCenterView 연결작업 및 더보기 수정

---
+++ CaZait-iOS/Views/CustomerCenterView.swift
@@ -0,0 +1,67 @@
+//
+//  CustomerCenterView.swift
+//  CaZait-iOS
+//
+//  Created by 강석호 on 2023/08/01.
+//
+
+import Foundation
+import UIKit
+
+class CustomerCenterView: UIViewController, UIGestureRecognizerDelegate {
+    
+    private let pinkView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 255, g: 223, b: 217)
+        return view
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+        // 네비게이션컨트롤러를 통해서 Status Bar 색깔 변경
+        self.navigationController?.navigationBar.barStyle = .black
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+        
+        view.addSubview(pinkView)
+        
+        self.pinkView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
+        
+        //손가락 옆으로 미는 제스쳐 작동
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        
+        // 네비게이션 바 타이틀 설정
+        self.title = "고객센터"
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        // 뒤로가기 버튼 추가
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonTapped))
+        self.navigationItem.leftBarButtonItem = backButton
+        // 내비게이션 바 스타일 변경
+        self.navigationController?.navigationBar.backgroundColor = .black
+        self.navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+}
