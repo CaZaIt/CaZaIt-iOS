44041b816206deb15f563edd498fd33704f766bb
alstn38 <alstn38@naver.com>
2023-08-06 17:26:05 +0900

    [Add] RecentModel 생성

---
+++ CaZait-iOS/Models/RecentModel.swift
@@ -0,0 +1,22 @@
+//
+//  RecentModel.swift
+//  CaZait-iOS
+//
+//  Created by 강민수 on 2023/08/06.
+//
+
+import Foundation
+
+struct RecentModel {
+    let cafeImage : String
+    let cafeName : String
+    let cafeLocation : String
+    let cafeCongestion : String
+    
+    init(cafeImage: String, cafeName: String, cafeLocation: String, cafeCongestion: String) {
+        self.cafeImage = cafeImage
+        self.cafeName = cafeName
+        self.cafeLocation = cafeLocation
+        self.cafeCongestion = cafeCongestion
+    }
+}
