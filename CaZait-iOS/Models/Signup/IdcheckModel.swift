472e6bbc5ff16a1e55d28f2d3ede217159c77c23
rkdtnlzl <rkdtnlzl@naver.com>
2023-08-29 19:30:10 +0900

    [Fix] 회원가입 model 및 api 통신코드 수정

--- CaZait-iOS/Models/Signup/IdcheckModel.swift
+++ CaZait-iOS/Models/Signup/IdcheckModel.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct IdCheckResponse: Codable {
     let code: Int
-    let data: String
+    let data: String?
     let message: String
     let result: String
 }
--- CaZait-iOS/Services/SignUp_Service/SignupService.swift
+++ CaZait-iOS/Services/SignUp_Service/SignupService.swift
@@ -13,7 +13,7 @@ import Alamofire
 class signupService {
     
     static let shared = signupService()
-//(2)싱글통 객체를 선언해서 앱 어디에서든지 접근가능하도록 한다
+    //(2)싱글통 객체를 선언해서 앱 어디에서든지 접근가능하도록 한다
     private init() {}
     //파라미터 변경(8/6)
     func signup(accountName: String, password: String, nickname: String, phoneNumber: String, completion: @escaping(NetworkResult<Any>) -> Void)
@@ -34,11 +34,11 @@ class signupService {
         //요청서 //Request 생성
         //통신할 주소, HTTP메소드, 요청방식, 인코딩방식, 요청헤더
         let dataRequest = AF.request(url,
-                                    method: .post,
-                                    parameters: body,
-                                    encoding: JSONEncoding.default,
-                                    headers: header)
-                                    
+                                     method: .post,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
         //request 시작 ,responseData를 호출하면서 데이터 통신 시작
         dataRequest.responseData{
             response in //데이터 통신의 결과가 response에 담기게 된다
@@ -65,15 +65,8 @@ class signupService {
     //통신이 성공하고 원하는 데이터가 올바르게 들어왔을때 처리하는 함수
     private func isVaildData(data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder() //서버에서 준 데이터를 Codable을 채택
-        do {
-            let decodedData = try decoder.decode(SignupResponse.self, from: data)
-            print("good")
-        } catch {
-            print("에러 출력")
-            print(error)
-        }
         guard let decodedData = try? decoder.decode(SignupResponse.self, from: data)
-        //데이터가 변환이 되게끔 Response 모델 구조체로 데이터를 변환해서 넣고, 그 데이터를 NetworkResult Success 파라미터로 전달
+                //데이터가 변환이 되게끔 Response 모델 구조체로 데이터를 변환해서 넣고, 그 데이터를 NetworkResult Success 파라미터로 전달
         else { return .pathErr }
         
         return .success(decodedData as Any)
